2fac3a81679502fc287b8a0b63884e770985561a
bot <kotohawk@gmail.com>
2022-05-06 23:44:13 +0200

    navbar cool highlight

--- frontend/first_fe_app/navbar/navbar.js
+++ frontend/first_fe_app/navbar/navbar.js
@@ -7,53 +7,80 @@ import {baseUrl, staticFolderUrl} from '../base/baseUrls.js';
 
 
 const NavbarButton = (props) => {
-	const [showDash, setShowDash] = useState(false);
+	const [highlight, setHighlight] = useState(false);
 	
-	const addDashOnTop = (e) => {
-		setShowDash(true);
+	const toHighlight = (e) => {
+		setHighlight(true)
 	}
 
-	const removeDashOnTop = (e) => {
-		setShowDash(false);
-	};
+	const unHighlight = (e) => {
+		setHighlight(false)
+	}
+
+	const onClick = (e) => {
+		if(props.onClick){
+			props.onClick(e)
+		}
+		
+		if(props.setClickedButtonID){
+			props.setClickedButtonID(props.id)
+		}
+	}
 
 	const navButtonProps = {
 		marginRight: '55px',
 		maxWidth: '200px', 
 		justifyContent: 'center', 
 		alignItems: 'center',
-		borderTop: showDash ? '0.3rem solid red': '',
-		onClick: props.onClick,
-		onMouseEnter: addDashOnTop,
-		onMouseLeave: removeDashOnTop
+		borderTop: props.isClicked || highlight ? '0.3rem solid #6aabe5': '',
+		onClick: onClick,
+		onMouseEnter: toHighlight,
+		onMouseLeave: unHighlight
 	}
 
 	return (
 		<Button {...navButtonProps}>
-			<NavbarText fontFamily={"'Rowdies', cursive"} fontSize={'20px'}>{props.children}</NavbarText>
+			<NavbarText fontFamily={"'Rowdies', cursive"} fontSize={'20px'} color={props.isClicked || highlight ? '#cee8ff': '#f9f9f9'}>
+				{props.children}
+			</NavbarText>
 		</Button>
 	)
 };
 
 
 const Navbar = (props) => {
+	const navbarPathToButtonIdMap = {
+		'/main-page/about-me/': 1,
+		'/main-page/tech/': 2,
+		'/main-page/projects/': 3,
+	}
 	const [showPopup, setShowPopup] = useState(false)
+	const [clickedButtonID, setClickedButtonID] = useState(navbarPathToButtonIdMap[window.location.pathname]);
+
 	const loginCtx = useContext(LoginStatusContext)
-	console.log('nv ctx:', loginCtx)
 	
+	console.log('nv ctx:', loginCtx)
+
+	const navbarContainerProps = {
+		backgroundColor: '#242528',
+		borderRadius: '0px 0px 10px 10px',
+		justifyContent: 'flex-end',
+		height: '70px'
+	}
+
+	// Navbar buttons
+	const navButtonProps = {
+		clickedButtonID: clickedButtonID,
+		setClickedButtonID: setClickedButtonID,
+	}
+
+	// Close login button
 	const showLoginPopup = (e) => {
 		setShowPopup(true)
 	}
 
 	const closeLoginPopup = (e) => {
 		setShowPopup(false)
-	}
-
-	const navbarContainerProps = {
-		backgroundColor: '#f0f5f5',
-		borderRadius: '0px 0px 10px 10px',
-		justifyContent: 'flex-end',
-		height: '70px'
 	}
 
 	const closeLoginPopupButtonProps = {
@@ -69,15 +96,15 @@ const Navbar = (props) => {
     	<StyledRow {...navbarContainerProps}>
 			<NavbarLogo src={`${staticFolderUrl}`}></NavbarLogo>
 			<RouterCustomLink to='/main-page/about-me/'>
-				<NavbarButton>About Me</NavbarButton>
+				<NavbarButton id={1} isClicked={clickedButtonID == 1} {...navButtonProps}>About Me</NavbarButton>
 			</RouterCustomLink>
 			<RouterCustomLink to='/main-page/tech/'>
-				<NavbarButton>Technologies</NavbarButton>
+				<NavbarButton id={2} isClicked={clickedButtonID == 2} {...navButtonProps}>Technologies</NavbarButton>
 			</RouterCustomLink>
 			<RouterCustomLink to='/main-page/projects/'>
-				<NavbarButton>Projects</NavbarButton>
+				<NavbarButton id={3} isClicked={clickedButtonID == 3} {...navButtonProps}>Projects</NavbarButton>
 			</RouterCustomLink>
-			<NavbarButton onClick={showLoginPopup}>Sign in</NavbarButton>
+			<NavbarButton isClicked={showPopup} onClick={showLoginPopup}>Sign in</NavbarButton>
 			{showPopup ? <LoginPopup closePopupButton={closeLoginPopupButton}></LoginPopup>: null}
  		</StyledRow>
  	)
