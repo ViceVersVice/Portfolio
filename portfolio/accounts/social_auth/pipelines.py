54c96a8a83606becd217f09afb62de73d2b241dc
bot <kotohawk@gmail.com>
2022-06-01 22:11:40 +0200

    fix picture url

--- portfolio/accounts/social_auth/pipelines.py
+++ portfolio/accounts/social_auth/pipelines.py
@@ -4,9 +4,8 @@ from accounts.models import UserProfile
 
 
 def create_user_profile(strategy, details, backend, user=None, *args, **kwargs):
-    picture_url = details.get('picture', {}).get('url')
+    picture_url = kwargs.get('response', {}).get('picture', {}).get('data', {}).get('url')
     user_profile, created = UserProfile.objects.get_or_create(user=user)
     if picture_url:
         user_profile.avatar = picture_url
         user_profile.save()
-
